ca750622f55472ee57ac363676300531bc5c42a2
tradinggod365 <tradinggod365@gmail.com>
2026-01-14 22:44:25 +0000

    Update supabaseClient.js

--- TradingOS/supabaseClient.js
+++ TradingOS/supabaseClient.js
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://kuxubwtuafuuquesgrbs.supabase.co,
+  'https://kuxubwtuafuuquesgrbs.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1eHVid3R1YWZ1dXF1ZXNncmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MTczMzgsImV4cCI6MjA4Mzk5MzMzOH0.CuHNq4mcmZfO-SNNTKI-bnDMibOIbQBl66nlke8jVrg'
 );
